9a98b49904e869d8e4665ce39db4feff43c5aa4c
hugoxxxxx <52855225+hugoxxxxx@users.noreply.github.com>
2024-01-07 09:15:20 +0800

    Update beifenmaotai.js

--- beifenmaotai.js
+++ beifenmaotai.js
@@ -29,10 +29,10 @@ var province = $.getdata('imaotai__config__province') || '' // 省份
 var city = $.getdata('imaotai__config__city') || '' // 城市
 var itemList = $.getdata('imaotai__config__itemcode')?.split(',') || ['10213', '10214'] // 预约项
 var itemMap = {
-    10213: '贵州茅台酒（癸卯兔年）',
+    10941: '贵州茅台酒（癸卯龍年）',
     10056: '53%vol 500ml 茅台1935',
     2478: '贵州茅台酒（珍品）',
-    10214: '贵州茅台酒（癸卯兔年）x2'
+    10942: '贵州茅台酒（癸卯龍年）x2'
 }
 var address = $.getdata('imaotai__config__address') || '' // 详细地址
 var location = $.getdata('imaotai__config__location') || '' // 地址经纬度
